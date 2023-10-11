95242c5f3e1001e651446d24f5515d53aa24ffc5
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2023-10-11 10:33:30 +0300

    222

--- src/componets/UserInfo/UserInfo.tsx
+++ src/componets/UserInfo/UserInfo.tsx
@@ -5,7 +5,14 @@ import { auth } from '../../store/MovieApi';
 import styles from './UserInfo.module.scss';
 import { MenuOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
+import {
+  DownOutlined,
+  UserOutlined,
+  HomeOutlined,
+  StarOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
+import { Button, Dropdown, Space } from 'antd';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -33,47 +40,53 @@ const UserInfo = () => {
     navigate('/login');
   };
   const items: MenuProps['items'] = [
-    getItem('Menu', 'sub1', <MenuOutlined />, [
-      getItem(
-        <Link className={styles.lin} to="/profile">
-          Profile
-        </Link>,
-        '5',
-      ),
-      getItem(
-        <Link className={styles.lin} to="/favorites">
-          Favorites
-        </Link>,
-        '3',
-      ),
-      getItem(<div onClick={exitFnc}>Exit</div>, '6'),
-    ]),
+    getItem(
+      <Link className={styles.lin} to="/">
+        Home
+      </Link>,
+      '1',
+      <HomeOutlined />,
+    ),
+    getItem(
+      <Link className={styles.lin} to="/profile">
+        Profile
+      </Link>,
+      '5',
+      <UserOutlined />,
+    ),
+    getItem(
+      <Link className={styles.lin} to="/favorites">
+        Favorites
+      </Link>,
+      '3',
+      <StarOutlined />,
+    ),
+    getItem(<div onClick={exitFnc}>Exit</div>, '6', <LogoutOutlined />),
+    ,
   ];
   const navigate = useNavigate();
   const { data: dataApi, refetch } = auth.useAuthApiQuery('');
   const dispatch = useAppDispatch();
 
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    console.log('click', e);
   };
-  const onOpenChange =(e:any)=>{
-    console.log('vv',e)
-  }
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
 
   return (
     <div className={styles.container}>
-      <div ref={refUser} className={styles.container__username}>{dataApi?.username}</div>
-      <Menu 
-      subMenuCloseDelay={0.1}
-        onOpenChange={onOpenChange}
-        onClick={onClick}
-        style={{ width: 112 }}
-        defaultSelectedKeys={[null]}
-        defaultOpenKeys={[null]}  
-        mode="vertical"
-        items={items}
-        className={styles.menu}
-      />
+      <Dropdown className={styles.menu} menu={menuProps}>
+        <Button>
+          <Space>
+            <MenuOutlined />
+            Menu
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
     </div>
   );
 };
